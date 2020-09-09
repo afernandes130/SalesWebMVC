14f88a7f78b75af751f5627afcfb6052531acbd4
Adriano Fernandes <afernandes130@hotmail.com>
2020-09-09 20:01:35 -0300

    Versão inicial do projeto

---
+++ SalesWebMVC/Models/Departament.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Models
+{
+    public class Departament
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void AddSeller() {}
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return 0.0;
+        }
+
+    }
+}
